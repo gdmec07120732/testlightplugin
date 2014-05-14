9f79eed40b8a15774df964d940b0f6d964f2ff5d
diki <819348195@qq.com>
2014-05-14 11:46:04 +0800

    提交

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,11 @@
+C:\testlightplugin\platforms\android\bin\classes.dex : \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\t00385\lightsensor\LightSensorListener$1.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\t00385\lightsensor\LightSensorListener.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\BuildConfig.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\R$attr.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\R$drawable.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\R$string.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\R$xml.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\R.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120xxx\testlightplugin\testlightplugin.class \
+C:\testlightplugin\platforms\android\bin\dexedLibs\classes-fc16f74325d8233d7e8d00ca88dba06b.jar \
